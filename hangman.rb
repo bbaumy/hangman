e85f213517fe124bfd1caef6f58b649d8dc84a3c
bBaumy <brian@boomtownroi.com>
2017-01-16 12:13:51 -0500

    Full dictionary of words working.

--- hangman.rb
+++ hangman.rb
@@ -2,10 +2,20 @@ class Hangman
   attr_accessor :words, :word
 
   def initialize(word=nil)
-    @words = ['banana', 'apple', 'technodrome']
-    @word = word
-    @word ||= @words.sample
-    # words = File.readlines '/usr/share/dict/words'
+    # @words = ['banana', 'apple', 'technodrome']
+    # @word = word
+    # @word ||= words.sample
+    @guesses = []
+    @turns = 7
+
+    contents    = File.read '/usr/share/dict/words'
+    words       = contents.split
+    good_words  = words.select do |w|
+      4 <= w.length && w.length <= 9 && w.downcase == w
+    end
+
+    @word = good_words.sample
+
   end
 
   def ask_for_user_input
@@ -16,25 +26,52 @@ class Hangman
       puts 'Correct!'
     else
       puts 'No!'
+      # Player loses a turn
+      @turns -= 1
     end
     @guesses.push letter
   end
 
-  def show_guesses
-   puts @guesses.join(',')
+  def show_board
+    array = []
+    @word.chars.each do |char|
+      if @guesses.include? char
+        array.push char
+      else
+        array.push '_'
+      end
+    end
+    array.join(' ')
   end
 
+  def show_guesses
+   puts @guesses.join(',')
+   puts "You have #{@turns} turns left"
+  end
+
+  def out_of_turns?
+    @turns == 0
+  end
+
+def found_answer?
+  # 1. the board includes no underscores
+  !show_board.include?('_')
+
+  # 2. every letter in the answer (@word) has been guessed
+  @word.chars.all? do |letter|
+    @guesses.include? letter
+  end
+  end
 end
 
+g = Hangman.new
+until g.out_of_turns? || g.found_answer? do
+  g.ask_for_user_input
+  puts g.show_board
+  g.show_guesses
+end
 
-
-
-
-# def take_turn
-#   @turns_left -= 1
-# end
-
-
+puts "The answer was: #{g.word}"
 
 
 # @turns_left = word.length
